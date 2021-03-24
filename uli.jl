21928158ad462223bb1598b857b1322913f5e60b
Uli Fahrenberg <ulrich.fahrenberg@inria.fr>
2021-03-24 15:56:08 +0100

    code for generating new gp-iposet files from old ones

--- uli.jl
+++ uli.jl
@@ -1,39 +1,51 @@
 include("posets.jl")
 include("iposets.jl")
-using StatProfilerHTML
+#using StatProfilerHTML
 
-"""Read a file of iposets and return them in an array"""
-function readIposets(filename)
-    res = Array{Iposet}(undef, 0)
-    open(filename, "r") do file
-        for line in eachline(file)
-            nums = split(line)
-            np = parse(Int, nums[1], base=16) # number of points
-            ne = parse(Int, nums[2], base=16) # number of edges
-            #println(nums, np, ne)
-            dg = SimpleDiGraph(np)
-            for edges_num in nums[3:ne+2]
-                add_edge!(dg, parse(Int, edges_num[1], base=16), parse(Int, edges_num[2], base=16))
-            end
-            if length(nums) == ne+2 # no sources neither targets
-                s, t = (), ()
-            elseif length(nums) == ne+3 # sources, but no targets
-                s = Tuple([parse(Int, x, base=16) for x in nums[ne+3]])
-                t = ()
-            elseif length(nums) == ne+4 && nums[ne+3] == "-" # targets, but no sources
-                s = ()
-                t = Tuple([parse(Int, x, base=16) for x in nums[ne+4]])
-            else # sources and targets
-                s = Tuple([parse(Int, x, base=16) for x in nums[ne+3]])
-                t = Tuple([parse(Int, x, base=16) for x in nums[ne+4]])
-            end
-            push!(res, Iposet(s, t, dg))
+"""Generate all gp-iposets with n nodes, k sources, l targets,
+using files in gpiposets/ for memo initialization"""
+function gpiPosetsMemo(n, k, l)
+    alliposets = Array{Array{Tuple{Iposet, Array{Tuple{Int, Int}}}}}(undef, (n*(n-1))÷2 + 1, n + 1, n + 1, n)
+    @inbounds for i in eachindex(alliposets)
+        alliposets[i] = []
     end
-        return res
+    filled = zeros(Bool, n + 1, n + 1, n)
+    locks = Array{ReentrantLock}(undef, (n*(n-1))÷2 + 2, n + 1, n + 1, n)
+    for i in eachindex(locks)
+        locks[i] = ReentrantLock()
     end
+    for np in 1:7
+        fname = string("gpiposets/gpi",np,".ips")
+        ips = loadIposets(fname)
+        for ip in ips
+            ipe = ne(ip.poset)
+            kp = length(ip.s)
+            lp = length(ip.t)
+            vprof = Array{Tuple{Int, Int}}(undef, np)
+            @inbounds for v in 1:np
+                vprof[v] = (inHash(ip.poset, v), outHash(ip.poset, v))
+            end
+            push!(alliposets[ipe + 1, kp + 1, lp + 1, np], (ip, vprof))
+        end
+        for kp in 0:np
+            for lp in 0:np
+                filled[kp + 1, lp + 1, np] = true
+            end
+        end
+    end
+    return gpiPosets(n, k, l, alliposets, filled, locks)
 end
 
-#ips = readIposets("test.txt")
-#println(length(ips), " ", ips[end])
 
-saveIposets(gpiPosets(7), "gpi7.ips")
+#saveIposets(gpiPosets(7), "gpi7.ips")
+#ips = loadIposets("gpiposets/gpi0.ips")
+#println(ips)
+
+n = 8
+k = 0
+l = 1
+fname = string("gpi",n,"-",k,"-",l,".ips")
+println(fname)
+ips = gpiPosetsMemo(n, k, l)
+println(length(ips))
+saveIposets(ips, fname)
